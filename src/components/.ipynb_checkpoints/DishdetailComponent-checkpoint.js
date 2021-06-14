d3cd5ca723caaae6cfa857328b27a8dda5c6de1f
Aareez <syedaareez7@gmail.com>
2021-06-15 01:02:50 +0530

    Presentation and container week2

--- src/components/.ipynb_checkpoints/DishdetailComponent-checkpoint.js
+++ src/components/.ipynb_checkpoints/DishdetailComponent-checkpoint.js
@@ -5,9 +5,6 @@ import './DishdetailComponent.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
     
     renderDish(dish) {
         if (dish != null)
@@ -25,13 +22,14 @@ class DishDetail extends Component {
                 <div></div>
             );
     };
-    renderComment(dish){
+
+     renderComment(dish){
         if(dish){
         const dishReturn = dish.comments.map((singleComment) => {
             return(
                 <li key={singleComment.id} >
                 <p>{singleComment.comment}</p>
-                <p>-- {singleComment.author} , {singleComment.date}</p>
+                <p>-- {singleComment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(singleComment.date)))}</p>
                 </li>
             );
         });
@@ -50,17 +48,19 @@ class DishDetail extends Component {
         
         
         return(
+            <div className="container">
             <div className="row">
                   <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.Dish)}
+                    {this.renderDish(this.props.dish)}
                   </div>
                     <div  className="col-12 col-md-5 m-1">
                         <h4 className="commentheading">Comments</h4>
                         <ul className="list-unstyled">
-                        {this.renderComment(this.props.Dish)}
+                        {this.renderComment(this.props.dish)}
                         </ul>
                       </div>
                 </div>
+            </div>
         );
     };
 
--- src/components/.ipynb_checkpoints/MenuComponent-checkpoint.js
+++ src/components/.ipynb_checkpoints/MenuComponent-checkpoint.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+
 
 class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-    this.state = {
-      detailBlockCall: null
-    }
-    }
-    
-    onClickBlock(dish) {
-        this.setState({ detailBlockCall: dish});
-    }
-     
-    renderDetailBlock(dish){
-        return(
-        <DishDetail Dish={dish} />
-    );
-    }
-        
-    
     
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card 
-                  onClick={() => this.onClickBlock(dish)}>
+                <Card onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -43,7 +23,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                {this.renderDetailBlock(this.state.detailBlockCall)}
             </div>
 
             
