0ae3edc1529b6db80e53bcea4008e87779f039b2
Aareez <syedaareez7@gmail.com>
2021-06-17 01:50:33 +0530

    React router

--- src/components/.ipynb_checkpoints/MainComponent-checkpoint.js
+++ src/components/.ipynb_checkpoints/MainComponent-checkpoint.js
@@ -3,7 +3,9 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -11,22 +13,25 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
 
   render() {
+      
+    const HomePage = () => { return(<Home />); }
+         
     return (
       <div>
         <Header />
         
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        
+            <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> 
+              <Redirect to="/home" />
+            </Switch>
+        
 
         <Footer />
 </div>
