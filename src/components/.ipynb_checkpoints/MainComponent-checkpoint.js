c56ea62d47b2029e59166ca454e1e20f8fc7fe5d
Aareez <syedaareez7@gmail.com>
2021-06-18 00:21:21 +0530

    SPA part 1

--- src/components/.ipynb_checkpoints/MainComponent-checkpoint.js
+++ src/components/.ipynb_checkpoints/MainComponent-checkpoint.js
@@ -5,6 +5,10 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,13 +17,24 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
 
   render() {
       
-    const HomePage = () => { return(<Home />); }
+    const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
          
     return (
       <div>
@@ -29,6 +44,7 @@ class Main extends Component {
             <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> 
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
             </Switch>
         
