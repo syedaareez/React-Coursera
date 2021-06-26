0790a4a4d691abe3510f8cfd0e857b784a14d822
Aareez <syedaareez7@gmail.com>
2021-06-26 22:09:56 +0530

    Intro to Redux

--- src/.ipynb_checkpoints/App-checkpoint.js
+++ src/.ipynb_checkpoints/App-checkpoint.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 
@@ -9,11 +13,13 @@ class App extends Component {
    
   render(){ 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
   );
 };
 }
